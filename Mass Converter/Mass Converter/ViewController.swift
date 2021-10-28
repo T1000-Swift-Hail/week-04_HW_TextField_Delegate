16746b143acb9e68461cd4999a366051724cffda
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-10-28 09:12:55 +0300

    H.w

---
+++ Mass Converter/Mass Converter/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  Mass Converter
+//
+//  Created by noyer altamimi on 20/03/1443 AH.
+//
+
+import UIKit
+
+
+enum ConversionType {
+    case KG
+    case Pound
+    
+}
+
+class ViewController: UIViewController,UITextFieldDelegate  {
+    
+    var conversionType = ConversionType.KG
+    
+    
+    @IBOutlet weak var conversionLabel: UILabel!
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    func convertToKG( pound : Double) -> Double{
+        
+        return pound*2.2
+    }
+    
+    func convertToPound( kg : Double) -> Double{
+        
+        return kg*0.45
+    }
+    
+    
+    
+    
+    
+    @IBAction func segmentedChanged(_ sender: Any) {
+        
+        inputTextField.text = ""
+        
+        switch segmentControl.selectedSegmentIndex {
+            
+        case 0:
+            conversionType = .KG
+            conversionLabel.text = "0.0 Pounds"
+        case 1:
+            conversionType = .Pound
+            conversionLabel.text = "0.0 KG"
+        default:
+            conversionType = .KG
+            conversionLabel.text = "0.0 Pounds"
+            
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        let tap = UITapGestureRecognizer( target: self , action: #selector(dismisskey))
+        
+        view.addGestureRecognizer(tap)
+        inputTextField.delegate = self
+    }
+    
+    func textField(_ textField: UITextField,shouldChangeCharactersIn rang:NSRange,replacementString string: String) -> Bool {
+        if  inputTextField.text?.count ?? 0 >= 6 {
+            //if  <= 6 { return true
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    @IBAction func textChanged(_ sender: Any) {
+        
+        var amountValue = 0.0
+        
+    }
+    
+    @objc func dismisskey() {
+        
+        inputTextField.resignFirstResponder()
+        
+        
+        
+        
+        
+        let userText = inputTextField.text ?? "0.0"
+        
+        let userTextDouble = Double(userText) ?? 0.0
+        
+        
+        
+        switch conversionType {
+        case .KG:
+            let convertedValue = convertToKG(pound: userTextDouble)
+            conversionLabel.text = "\(convertedValue) Pounds"
+        case .Pound:
+            let convertedValue = convertToPound(kg: userTextDouble)
+            conversionLabel.text = "\(convertedValue) KG"
+        }
+        
+        
+    }
+}
+
+
